932adbff70a86fd1e79359d8323761ef569e5ca0
Henrik Sandström <sandstrom.henrik@gmail.com>
2017-05-17 13:19:23 +0200

    Update according to wep changes

--- log.go
+++ log.go
@@ -2,23 +2,11 @@ package wrapplog
 
 import (
 	"os"
-	"strings"
 
 	"github.com/Sirupsen/logrus"
 )
 
-var jsonFormatter = logrus.JSONFormatter{}
-
-type WrappFormatter struct{}
-
 func init() {
-	logrus.SetFormatter(&WrappFormatter{})
+	logrus.SetFormatter(&logrus.JSONFormatter{})
 	logrus.SetOutput(os.Stdout)
-}
-
-// Format logs according to WEP-007
-func (f *WrappFormatter) Format(entry *logrus.Entry) ([]byte, error) {
-	jsonBytes, err := (&jsonFormatter).Format(entry)
-	prefix := []byte(strings.ToUpper(entry.Level.String()) + " ")
-	return append(prefix[:], jsonBytes[:]...), err
 }
